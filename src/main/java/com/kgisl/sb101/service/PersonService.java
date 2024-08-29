8dfe944b312987471b73eda5751ca8a4fa831d1c
Pavithra V <pavithra.v@kgisl.com>
2024-08-29 17:51:55 +0530

    first commit

---
+++ src/main/java/com/kgisl/sb101/service/PersonService.java
@@ -0,0 +1,17 @@
+package com.kgisl.sb101.service;
+import java.util.List;
+
+import com.kgisl.sb101.entity.Person;
+
+public interface PersonService {
+ 
+    List<Person> getAllPersons();
+ 
+    Person createPerson(Person person);
+ 
+    Person getPersonById(int id);
+ 
+    Person updatePerson(int id, Person updatedPerson);
+ 
+    void deletePerson(int id);
+}
