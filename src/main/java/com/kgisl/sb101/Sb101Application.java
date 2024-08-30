ec3d91a5ef9c9a755576f082248954857f0b725b
Pavithra V <pavithra.v@kgisl.com>
2024-08-30 12:41:44 +0530

    made changes

--- src/main/java/com/kgisl/sb101/Sb101Application.java
+++ src/main/java/com/kgisl/sb101/Sb101Application.java
@@ -2,7 +2,9 @@ package com.kgisl.sb101;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
 @SpringBootApplication
 public class Sb101Application {
 
